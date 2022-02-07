7d6bd05fb4447660a25ed542f68f436a809284ff
Joseph Torsella <joseph.r.torsella@gmail.com>
2022-02-07 09:14:59 -0500

    Initialize minimal vimrc

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,10 @@
+local colorscheme = "material"
+
+-- Only for material colorscheme
+vim.g.material_style = "darker"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
