f277906a637110fa4911922c106895d00028217f
Joseph Torsella <joseph.r.torsella@gmail.com>
2022-02-06 20:15:35 -0500

    fix: prevent startup bug

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -41,6 +41,6 @@ for k, v in pairs(options) do
   vim.opt[k] = v
 end
 
-vim.cmd "set whichwrap+=<,>,[,],h,1"
+vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 -- vim.cmd [[set formatoptions-=cro]] -- TODO: What does this do and why doesn't it work?
