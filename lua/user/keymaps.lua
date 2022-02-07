9a5ff843b1de40f4a1e354fac38e771655b92311
Joseph Torsella <joseph.r.torsella@gmail.com>
2022-02-06 20:10:14 -0500

    Add keymaps and options to create boilerplate nvimluarc

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,49 @@
+-- This config owes a great deal to LunarVim/Neovim-from-scratch
+
+local opts = { noremap = true, silent = true }
+
+local keymap = vim.api.nvim_set_keymap
+
+--Set leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Normal Mode --
+
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Switching buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Move a block of text
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+
+-- Visual Mode --
+-- Indent text
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text
+keymap("v", "<A-j>", "<Esc>:m .+1<CR>==", opts)
+keymap("v", "<A-k>", "<Esc>:m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
---
+++ lua/user/options.lua
@@ -0,0 +1,46 @@
+local options = {
+  backup = false,
+  clipboard = "unnamedplus",
+  cmdheight = 2,
+  completeopt = { "menuone", "noselect" },
+  conceallevel = 0,
+  fileencoding = "utf-8",
+  hlsearch = true, --TODO - timeout for highlighting all matches
+  ignorecase = true,
+  mouse = "", -- disable mouse in all modes
+  pumheight = 10, -- pum === popup menu
+  showmode = true,
+  showtabline = 2,
+  smartcase = true, -- disable ignorecase if upper case char in search
+  smartindent = true, -- TODO: figure out difference between this and autoindent
+  splitbelow = true,
+  splitright = true,
+  swapfile = false, -- TODO: what is a swapfile and why am I disabling it?
+  -- termguicolors = true,
+  timeoutlen = 100,
+  undofile = true,
+  -- updatetime = 300, -- 4000ms default update time for completion. Which do I want?
+  writebackup = false, -- TODO: what does this do?
+  expandtab = true,
+  shiftwidth = 2,
+  tabstop = 2,
+  cursorline = true,
+  number = true,
+  relativenumber = true,
+  numberwidth = 4,
+  signcolumn = "yes",
+  wrap = true,
+  scrolloff = 8,
+  sidescrolloff = 8,
+  guifont = "MesloLGS NF:h16",
+}
+
+vim.opt.shortmess:append "c" -- TODO: why the linting error?
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+vim.cmd "set whichwrap+=<,>,[,],h,1"
+vim.cmd [[set iskeyword+=-]]
+-- vim.cmd [[set formatoptions-=cro]] -- TODO: What does this do and why doesn't it work?
